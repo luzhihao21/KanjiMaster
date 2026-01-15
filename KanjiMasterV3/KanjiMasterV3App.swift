98ccb078103eab8e3d35e183a5727e6cce20d041
luzhihao <luzhihao21@gmail.com>
2026-01-16 00:33:47 +0900

    Initial Commit

---
+++ KanjiMasterV3/KanjiMasterV3App.swift
@@ -0,0 +1,17 @@
+//
+//  KanjiMasterV3App.swift
+//  KanjiMasterV3
+//
+//  Created by Michael on 2026/01/16.
+//
+
+import SwiftUI
+
+@main
+struct KanjiMasterV3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
